1373b2389294fa2242fba934545dbf75150be263
Aditya Vikram <adityavikram2016@gmail.com>
2020-03-17 15:59:00 +0530

    Add project files.

---
+++ vbAcrylic/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TrackBar2 = New System.Windows.Forms.TrackBar()
+        Me.Panel1.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(487, 545)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 15.25!)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(151, 253)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(168, 33)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Acrylic4Winforms"
+        Me.Label1.UseCompatibleTextRendering = True
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel4.Controls.Add(Me.Label3)
+        Me.Panel4.Controls.Add(Me.TrackBar2)
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel4.Location = New System.Drawing.Point(487, 0)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(313, 545)
+        Me.Panel4.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!)
+        Me.Label3.Location = New System.Drawing.Point(27, 232)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 22)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Opacity"
+        Me.Label3.UseCompatibleTextRendering = True
+        '
+        'TrackBar2
+        '
+        Me.TrackBar2.Location = New System.Drawing.Point(27, 265)
+        Me.TrackBar2.Maximum = 254
+        Me.TrackBar2.Name = "TrackBar2"
+        Me.TrackBar2.Size = New System.Drawing.Size(263, 45)
+        Me.TrackBar2.TabIndex = 1
+        Me.TrackBar2.TickFrequency = 5
+        Me.TrackBar2.Value = 100
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 545)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TopMost = True
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TrackBar2 As TrackBar
+End Class
---
+++ vbAcrylic/Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Windows.Interop
+
+Public Class Form1
+    Private Declare Sub keybd_event Lib "user32" (ByVal bVk As Byte, ByVal bScan As Byte, ByVal dwFlags As Long, ByVal dwExtraInfo As Long)
+    Private Const KEYEVENTF_KEYUP = &H2
+    Private Const VK_LWIN = &H5B
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        HideAllOtherWindows()
+        EnableBlur()
+        Refresh()
+    End Sub
+    Public Sub HideAllOtherWindows()
+        keybd_event(VK_LWIN, 0, 0, 0)
+        keybd_event(Keys.Home, 0, 0, 0)
+        keybd_event(Keys.Home, 0, KEYEVENTF_KEYUP, 0)
+        keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0)
+    End Sub
+    Protected Overrides ReadOnly Property CreateParams As CreateParams
+        Get
+            Const CS_DROPSHADOW As Integer = &H20000
+            Dim cp As CreateParams = MyBase.CreateParams
+            cp.ClassStyle = cp.ClassStyle Or CS_DROPSHADOW
+            Return cp
+        End Get
+    End Property
+
+    Friend Enum AccentState
+        ACCENT_DISABLED = 0
+        ACCENT_ENABLE_GRADIENT = 1
+        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2
+        ACCENT_ENABLE_BLURBEHIND = 3
+        ACCENT_ENABLE_ACRYLICBLURBEHIND = 4
+        ACCENT_INVALID_STATE = 5
+    End Enum
+
+    <StructLayout(LayoutKind.Sequential)>
+    Friend Structure AccentPolicy
+        Public AccentState As AccentState
+        Public AccentFlags As Integer
+        Public GradientColor As Integer
+        Public AnimationId As Integer
+    End Structure
+
+    <StructLayout(LayoutKind.Sequential)>
+    Friend Structure WindowCompositionAttributeData
+        Public Attribute As WindowCompositionAttribute
+        Public Data As IntPtr
+        Public SizeOfData As Integer
+    End Structure
+
+    Friend Enum WindowCompositionAttribute
+        ' ...
+        WCA_ACCENT_POLICY = 19
+        ' ...
+    End Enum
+
+    <DllImport("user32.dll")>
+    Friend Shared Function SetWindowCompositionAttribute(ByVal hwnd As IntPtr, ByRef data As WindowCompositionAttributeData) As Integer
+    End Function
+
+    Private _blurOpacity As UInteger
+
+    Public Property BlurOpacity As Double
+        Get
+            Return _blurOpacity
+        End Get
+        Set(ByVal value As Double)
+            _blurOpacity = CUInt(value)
+            EnableBlur()
+        End Set
+    End Property
+    Private _blurBackgroundColor As UInteger = &HFFFFFF
+
+    Friend Sub EnableBlur()
+        Dim windowHelper = Me
+        Dim accent = New AccentPolicy()
+        accent.AccentState = AccentState.ACCENT_ENABLE_ACRYLICBLURBEHIND
+        accent.GradientColor = _blurBackgroundColor
+        accent.AccentFlags = &H20 Or &H40 Or &H80 Or &H100
+        Dim accentStructSize = Marshal.SizeOf(accent)
+        Dim accentPtr = Marshal.AllocHGlobal(accentStructSize)
+        Marshal.StructureToPtr(accent, accentPtr, False)
+        Dim data = New WindowCompositionAttributeData()
+        data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY
+        data.SizeOfData = accentStructSize
+        data.Data = accentPtr
+        SetWindowCompositionAttribute(windowHelper.Handle, data)
+        Marshal.FreeHGlobal(accentPtr)
+    End Sub
+
+    Private Sub TrackBar2_Scroll(sender As Object, e As EventArgs) Handles TrackBar2.Scroll
+        Panel1.BackColor = Color.FromArgb(TrackBar2.Value, 0, 0, 0)
+    End Sub
+    Dim IsMouseDown As Boolean
+    Dim startPoint
+
+    Private Sub Panel4_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel4.MouseUp
+        IsMouseDown = False
+    End Sub
+
+    Private Sub Panel4_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel4.MouseMove
+        If IsMouseDown Then
+            Dim p1 = New Point(e.X, e.Y)
+            Dim p2 = PointToScreen(p1)
+            Dim p3 = New Point(p2.X - startPoint.X, p2.Y - startPoint.Y)
+            Location = p3
+            Opacity = 0.95
+        End If
+    End Sub
+
+    Private Sub Panel4_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles Panel4.MouseDown
+        If e.Button = MouseButtons.Left Then
+            Me.Panel4.Capture = False
+            Const WM_NCLBUTTONDOWN As Integer = &HA1S
+            Const HTCAPTION As Integer = 2
+            Dim msg As Message = Message.Create(Me.Handle, WM_NCLBUTTONDOWN, New IntPtr(HTCAPTION), IntPtr.Zero)
+            Me.DefWndProc(msg)
+        End If
+    End Sub
+
+
+End Class
